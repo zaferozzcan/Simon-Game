b3e67ff7aba7e5b339ac1f2e0fddf89bc7e3b843
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-10-10 13:05:58 -0400

    user can see random seq, and can push his seq

--- js/app.js
+++ js/app.js
@@ -1,16 +1,39 @@
 // alert("JS IS ON")
 // console.log(typeof $());
+
+
+
 $(() => {
 
     let gameStuff = {
         gameFunctions: {
-
+            buttonClick: (e) => {
+                $(e.target).fadeOut(100).fadeIn(100);
+                gameStuff.userGuessSeq.push($(e.target))
+            },
+            seqEffect: (button) => {
+                $(button).fadeOut(100).fadeIn(100);
+            },
+            createRandom: () => {
+                let random = Math.floor(Math.random() * 4)
+                return random
+            },
+            startSeq: () => {
+                let randomButton = gameStuff.buttonArray[gameStuff.gameFunctions.createRandom()]
+                gameStuff.gameFunctions.seqEffect(randomButton);
+                gameStuff.gameSeqArray.push(randomButton)
+            },
+            makeNewSeq: () => {
+                // if game is on 
+                let randomButton = gameStuff.buttonArray[gameStuff.gameFunctions.createRandom()]
+                gameStuff.gameFunctions.seqEffect(randomButton);
+                gameStuff.gameSeqArray.push(randomButton)
+            }
         },
         buttonArray: [".top-right", ".top-left", ".bottom-left", ".bottom-right"],
-
+        gameSeqArray: [],
+        userGuessSeq: []
     }
-
-
 
 
 
