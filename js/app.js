fc0686741aad64c71c32dec167381e4033180617
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-10-10 12:05:11 -0400

    add js test

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// alert("JS IS ON")
+// console.log(typeof $());
+$(() => {
+
+    let gameStuff = {
+        gameFunctions: {
+
+        },
+        buttonArray: [".top-right", ".top-left", ".bottom-left", ".bottom-right"],
+
+    }
+
+
+
+
+
+});
